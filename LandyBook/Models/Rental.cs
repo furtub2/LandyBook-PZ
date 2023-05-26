78457b1aaaff7fba918d896afa605d3dfaa17e90
furtub2 <vladyslav.ishchuk.2@gmail.com>
2023-05-26 13:39:53 +0200

    add Rental Model

---
+++ LandyBook/Models/Rental.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace LandyBook.Models
+{
+    public class Rental
+    {
+        public int ID { get; set; }
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+        public IdentityUser User { get; set; }
+
+
+        [Display(Name = "Rental date")]
+        public DateTime RentDate { get; set; }
+
+        [Display(Name = "Return date")]
+        public DateTime ReturnDate { get; set; }
+
+        public bool isAccepted { get; set; }
+        public string Status { get; set; }
+        public bool IsCancelled { get; set; }
+    }
+}
+
---
+++ LandyBook/Models/RentalDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace LandyBook.Models
+{
+    public class RentalDTO
+    {
+
+        public int Id { get; set; }
+
+        [DisplayName("Rent Date")]
+        public DateTime RentDate { get; set; }
+
+        [DisplayName("Return Date")]
+        public DateTime ReturnDate { get; set; }
+
+    }
+}
+
