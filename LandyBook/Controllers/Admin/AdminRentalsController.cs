fddc60e0464c0e239707bc150b175f787a5a4067
furtub2 <vladyslav.ishchuk.2@gmail.com>
2023-06-02 14:13:49 +0200

    Add Admin Controllers

---
+++ LandyBook/Controllers/Admin/AdminRentalsController.cs
@@ -0,0 +1,119 @@
+﻿using LandyBook.Data;
+using LandyBook.Models;
+using LandyBook.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandyBook.Controllers.Admin
+{
+    [Authorize(Roles = "SuperAdmin")]
+    public class AdminRentalsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminRentalsController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IdentityUser user = await _userManager.FindByIdAsync("c64fe004-2784-4733-bc9f-5afddb458225");
+
+            var rentals = await _context.Rental
+                .Select(rental => new
+                {
+                    id = rental.ID,
+                    book = _context.Book.Where(book => book.ID == rental.BookId).Select(book => book.Title).FirstOrDefault(),
+                    rentDate = rental.RentDate.ToShortDateString(),
+                    returnDate = rental.ReturnDate.ToShortDateString(),
+                    status = rental.Status,
+                    isAccepted = rental.isAccepted,
+                    isCancelled = rental.IsCancelled,
+                    user = rental.User
+                })
+                .ToListAsync();
+
+            var rentalsWithUser = rentals.Select(async rental => new
+            {
+                id = rental.id,
+                book = rental.book,
+                rentDate = rental.rentDate,
+                returnDate = rental.returnDate,
+                status = rental.status,
+                user = await _userManager.FindByIdAsync(rental.user.Id),
+                isAccepted = rental.isAccepted,
+                isCancelled = rental.isCancelled
+            });
+
+            ViewBag.Rentals = await Task.WhenAll(rentalsWithUser);
+            return View();
+        }
+
+
+
+
+
+
+
+        public async Task<IActionResult> Accept(int? id)
+        {
+            var rental = _context.Rental.Where(rental => rental.ID == id).FirstOrDefault();
+            if (rental == null)
+            {
+                return NotFound();
+            }
+            rental.isAccepted = true;
+            rental.Status = "Accepted";
+            if (ModelState.IsValid)
+            {
+                _context.Update(rental);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Cancel(int? id)
+        {
+            var rental = _context.Rental.Where(rental => rental.ID == id).FirstOrDefault();
+            if (rental == null)
+            {
+                return NotFound();
+            }
+            rental.IsCancelled = true;
+            rental.Status = "Canceled";
+            if (ModelState.IsValid)
+            {
+                _context.Update(rental);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Remove(int? id)
+        {
+            var rental = _context.Rental.Where(rental => rental.ID == id).FirstOrDefault();
+            if (rental == null)
+            {
+                return NotFound();
+            }
+            rental.Status = "Removed";
+            if (ModelState.IsValid)
+            {
+                _context.Update(rental);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LandyBook/Controllers/Admin/BookController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LandyBook.Data;
+using LandyBook.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using LandyBook.Data;
+
+namespace LandyBook.Controllers.Admin
+{
+    [Authorize(Roles = "SuperAdmin")]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Book.Where(book => !book.isRemoved);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Author")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                book.IsAvailable = true;
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,Author,IsAvailable")] Book book)
+        {
+            if (id != book.ID)
+            {
+                return NotFound();
+            }
+            Console.WriteLine(ModelState.IsValid);
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Book == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Book'  is null.");
+            }
+            var book = await _context.Book.FindAsync(id);
+            book.isRemoved = true;
+            book.IsAvailable = false;
+            if (book != null)
+            {
+                _context.Book.Update(book);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+            return (_context.Book?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
